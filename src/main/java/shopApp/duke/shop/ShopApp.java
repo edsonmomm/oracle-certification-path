ddc5e38a0105b3d15f2ad978cddb56cf065fb452
Edson Momm <edson@agide.com.br>
2023-11-09 14:36:32 -0300

    Primitive types

--- src/main/java/shopApp/duke/shop/ShopApp.java
+++ src/main/java/shopApp/duke/shop/ShopApp.java
@@ -3,6 +3,7 @@ package shopApp.duke.shop;
 import shopApp.duke.choice.Customer;
 import shopApp.duke.shop.lessons.Lesson1Welcome;
 import shopApp.duke.shop.lessons.Lesson3_1;
+import shopApp.duke.shop.lessons.Lesson3_primitiveTypes;
 
 public class ShopApp {
     public static void main(String[] args) {
@@ -12,5 +13,10 @@ public class ShopApp {
         // Lesson 3.1 - Creates a customer with name
         Customer customer1 = Lesson3_1.createCustomerAndPrintName("Pinky");
         customer1.printGreetings();
+
+        // Primitive types
+        //Lesson3_primitiveTypes lesson3 = new Lesson3_primitiveTypes();
+        //System.out.println(lesson3.toString());
+
     }
 }
---
+++ src/main/java/shopApp/duke/shop/lessons/Lesson3_primitiveTypes.java
@@ -0,0 +1,43 @@
+package shopApp.duke.shop.lessons;
+
+/**
+ * Declarations and initialization of primitive types.
+ * There are also some examples of wrong declarations and initialization
+ */
+public class Lesson3_primitiveTypes {
+
+    // integral types
+    byte varByte = 1;
+    int varInt1 = 2; int varInt2 = 3;
+    short varShort = 4;
+    long varLong = 5;
+
+    // floating point types
+    float varFloat = 10.54f;
+    double varDouble = 11.5;
+
+    // Constants
+    final int NUMBER_OF_MONTHS = 12;
+
+    // Wrong declarations
+    //int varInt1 = 1; varInt2 = 2;
+    //string varString;
+    //float varFloat = 10.54;
+
+    @Override
+    public String toString() {
+        // NUMBER_OF_MONTHS = 13; // wrong, it's final, can't be changed
+
+        String breakLine = "\n";
+        StringBuilder strReturn = new StringBuilder("Primitive types:").append(breakLine);
+        strReturn.append("byte = ").append(varByte).append(breakLine);
+        strReturn.append("int = ").append(varInt1).append(breakLine);
+        strReturn.append("short = ").append(varShort).append(breakLine);
+        strReturn.append("long = ").append(varLong).append(breakLine);
+        strReturn.append("float = ").append(varFloat).append(breakLine);
+        strReturn.append("double = ").append(varDouble).append(breakLine);
+        strReturn.append("constant (number of months) = ").append(NUMBER_OF_MONTHS);
+
+        return strReturn.toString();
+    }
+}
