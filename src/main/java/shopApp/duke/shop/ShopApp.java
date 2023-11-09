31d2c99020cd143f2e815b82368b1d5157544270
Edson Momm <edson@agide.com.br>
2023-11-09 10:59:14 -0300

    First commit

---
+++ src/main/java/shopApp/duke/shop/ShopApp.java
@@ -0,0 +1,11 @@
+package shopApp.duke.shop;
+
+import shopApp.duke.shop.lessons.Lesson1Welcome;
+
+public class ShopApp {
+    public static void main(String[] args) {
+        // Prints the Duke's Shop welcome message
+        Lesson1Welcome.printWelcomeMessage();
+
+    }
+}
---
+++ src/main/java/shopApp/duke/shop/lessons/Lesson1Welcome.java
@@ -0,0 +1,13 @@
+package shopApp.duke.shop.lessons;
+
+/**
+ * Content from lesson "What is a Java Program"
+ */
+public class Lesson1Welcome {
+    /**
+     * Prints a welcome message for Duke's Shop
+     */
+    public static void printWelcomeMessage() {
+        System.out.println("Welcome to Duke Choice Shop.");
+    }
+}
